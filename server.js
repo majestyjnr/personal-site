7c6106afb9fb55f625a719213a9a6ae0d2c96614
Dr. Aidoo <aidoojuniorsolomon@gmail.com>
2020-11-14 16:34:53 -0800

    New Commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = new express();
+
+// Setting up the View Engine
+app.set("view engine", "ejs");
+
+// Serving Static content
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', function(req, res){
+    res.render('index');
+})
+
+// Launch the server
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, function () {
+  console.log(`Server running on PORT ${PORT}`);
+});
