478dbd19cfe9854f85f2a9da682ad281c8112734
Dr. Aidoo <aidoojuniorsolomon@gmail.com>
2020-11-14 16:43:10 -0800

    New Commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', function(req, res){
-    res.render('index');
+    res.render('./index')
 })
 
 // Launch the server
